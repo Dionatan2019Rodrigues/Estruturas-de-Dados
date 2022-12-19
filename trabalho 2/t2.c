18ec0d49571c0ce116ef281b931b50167544bc77
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2022-12-19 17:41:06 -0300

    leitura de msg

--- trabalho 2/t2.c
+++ trabalho 2/t2.c
@@ -1,9 +1,33 @@
 /** Dionatan Eduardo Correa Rodrigues **/
 #include <stdio.h>
 
+struct stBaralho{
+    int dado;
+    struct stBaralho *next;
+};
+
+FILE* leitura_mensagem(FILE * arq){
+
+    char msg[5];
+
+    arq = fopen("input", "r");
+    if (arq != NULL) {
+      for (int i=0; i<5; i++) {
+        fscanf(arq, "%c", &msg[i]);
+        printf("%c\t",msg[i]);
+      }
+      printf("\n");
+      
+    }
+
+    return arq;
+}
+
 int main(){
+    FILE *arq;
 
+    arq = leitura_mensagem(arq);   
 
-
+    
     return 0;
 }
