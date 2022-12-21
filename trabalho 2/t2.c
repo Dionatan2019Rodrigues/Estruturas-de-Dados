30abf35fbd49a0ee2d7a7bb20f0cbf83566b8eb8
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2022-12-21 16:28:52 -0300

    realizar solitaire

--- trabalho 2/t2.c
+++ trabalho 2/t2.c
@@ -1,22 +1,31 @@
 /** Dionatan Eduardo Correa Rodrigues **/
 #include <stdio.h>
-#include <stdlib.h> // used malloc() in function insertEnd()
+#include <stdlib.h> // used malloc() in functions insertEnd(),allocationMemoryCard() and createDeck()
 #include <stdbool.h> // used bool in function validateLetter()
 #include <ctype.h> // uded isalpha() in function validateLetter()
 
-int tam = 12;
+/*
+NAIPE VALUE
+PAUS    1
+OUROS   2
+COPAS   3
+ESPADAS 4
+JOKER   5
+*/
 
-struct stBaralho{
-    int dado;
-    struct stBaralho *next;
+struct stCard{
+    int index;
+    int value;
+    int naipe;
+    struct stCard *next;
 };
 
 struct stMsg{
-    char letra;
+    char letter;
     struct stMsg *next;
 };
 
-struct stMsg *insertEnd(char caracter, struct stMsg *list){
+struct stMsg *insertLetterEnd(char caracter, struct stMsg *list){
 
   if(list == NULL){
     struct stMsg *new;
@@ -25,34 +34,33 @@ struct stMsg *insertEnd(char caracter, struct stMsg *list){
     if(new == NULL) //ERROR memory allocation
      return NULL;
 
-    new->letra = caracter; // insert
+    list = new;
+    new->letter = caracter; // insert
     new->next = NULL;
   }else{
-    list->next = insertEnd(caracter,list->next);
+    list->next = insertLetterEnd(caracter,list->next);
   }
-
 
   return list;
 
 }
 
-void imprimeList(struct stMsg *list){
+void showListLetter(struct stMsg *list){
   struct stMsg *aux;
   aux = list;
   while(aux != NULL){
-    printf("%c \t", aux->letra);
+    printf("%c \t", aux->letter);
     aux = aux->next;
   }
   printf("\n");
 }
-
+// retorna true caso seja uma letra
 bool validateLetter(char caracter){
   if(isalpha(caracter) == 0) // retorna 0 para qualquer caracter que nao esta no alfabeto
     return false; // nao esta no alfabeto
   else 
     return true; // letra minuscula ou MAIUSCULA
 }
-
 //Converte char para MAIÚSCULA
 char convertToUppercase(char caracter){
   if((caracter >= 'a')&&(caracter <= 'z'))
@@ -64,32 +72,103 @@ char convertToUppercase(char caracter){
 struct stMsg *le_msg(struct stMsg *list){
   FILE *arq;
   char caracter;
+  int max_char = 150;
+
   arq = fopen("input", "r");
   if (arq != NULL) {
-    for (int i=0; i<tam; i++) {
+    for (int i=0; i < max_char; i++) { 
       fscanf(arq, "%c", &caracter);
       if(validateLetter(caracter)){
         caracter = convertToUppercase(caracter);
-        //printf("%c\t",caracter);
-        list = insertEnd(caracter,list);
+        list = insertLetterEnd(caracter,list);
       }   
-    } 
-    //printf("\n");     
+    }      
   }
 
-  //imprimeList(list);
   fclose(arq);
   return list;
+}
+// recebe um char e retorna o valor numérico de  1 a 26
+int letterValue(char c){
+  for(int i=0;i < 26;i++){
+    if(c == ('a'+i))
+      return i+1;
+  }
+  return 0;
+}
+
+struct stCard *allocationMemoryCard(struct stCard *ptr){
+  ptr = (struct stCard *) malloc(sizeof(struct stCard));
+  if(ptr == NULL) //ERROR memory allocation
+    return NULL;
+  else 
+    return ptr;
+}
+
+struct stCard *createJoker(struct stCard *ptr, int id){
+  ptr->index = id;
+  ptr->value = 14;
+  ptr->naipe = 5;
+  return ptr;
+}
+//cria baralho na posição inicial das cartas
+struct stCard *createDeck(struct stCard *list){
+  int k=0;
+  struct stCard *aux;
+
+  aux = (struct stCard *) malloc(sizeof(struct stCard));
+  if(aux == NULL) //ERROR memory allocation
+    return NULL;
+  list = aux;
+
+  for(int i=1;i < 5;i++){ // idenficador NAIPE
+    for(int j=1;j < 14;j++){  // idenficador Card (às,dois,três...)
+      aux->index = k+1;
+      aux->value = j;
+      aux->naipe = i;
+      aux->next = allocationMemoryCard(aux->next);
+      aux = aux->next;
+      k++;
+    }
+  }
+  //Joker A 
+  aux = createJoker(aux,53);
+  aux->next = allocationMemoryCard(aux->next);
+  aux = aux->next;
+  //Joker B
+  aux = createJoker(aux,54);
+  aux->next = NULL;
+
+
+  return list;
+}
+
+void showDeck(struct stCard *list){
+  struct stCard *aux;
+  aux = list;
+  printf("index\tvalue\tnaipe\n");
+  while(aux != NULL){
+    printf("%d \t", aux->index);
+    printf("%d \t", aux->value);
+    printf("%d \t", aux->naipe);
+    printf("\n");
+    aux = aux->next;
+  }
+  printf("\n");
 }
 
 
 
 int main(){
     struct stMsg *ptr_msg = NULL; //cria lista vazia 
+    struct stCard *ptr_card = NULL; //cria baralho vazio
 
     ptr_msg = le_msg(ptr_msg);
 
-    //imprimeList(ptr_msg);
+    ptr_card = createDeck(ptr_card);
+    
+    //showDeck(ptr_card);
+    //showListLetter(ptr_msg);
 
     
     return 0;
