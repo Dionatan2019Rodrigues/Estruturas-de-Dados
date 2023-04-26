0f3a840dbb3ebb8ee5d364aa7330d57ced5c1ee2
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2023-04-26 13:29:26 -0300

    Create exe01.c

---
+++ T1/exe01.c
@@ -0,0 +1,17 @@
+/*Desenvolvido por Dionatan Rodrigues e Mateus Quadros*/
+#include <stdio.h>
+
+typedef struct diagonal {
+    int ordem; 
+    int* v; 
+} diagonal;
+
+int main()
+{
+       
+
+
+    return 0;
+
+}
+
