d5c727cf6f988b7ef454372a0325fc39be2a5d44
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2023-05-03 14:04:38 -0300

    segmentation

--- T1/exe01.c
+++ T1/exe01.c
@@ -2,24 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- struct identidade {
-    int value;
+ struct lista {
     int row; 
     int column;
-    struct identidade *next;
+    int value;
+    struct lista *next;
     
 };
 
-int le_ordem(){
+struct identidade {
     int ordem;
-    printf("Digite a ordem de sua matriz identidade: ");
-    scanf("%d",&ordem);
-    return ordem;
-}
+    struct lista *prim;
+};
 
-struct identidade *allocation_memory(struct identidade *ptr){
+struct lista *allocation_memory(struct lista *ptr){
  
-  ptr = (struct identidade *) malloc(sizeof(struct identidade));
+  ptr = (struct lista *) malloc(sizeof(struct lista));
   if(ptr == NULL){ //ERROR memory allocation
     printf("ERRO DE ALOCAÇÃO");
     return NULL;
@@ -28,12 +26,60 @@ struct identidade *allocation_memory(struct identidade *ptr){
     
 }
 
-struct identidade *create_matriz(struct identidade *ptr_mat, int ordem){
+struct identidade *le_dados(struct identidade *ptr_mat, int ordem){
+
+    printf ("passagem\n");
+    ptr_mat->prim = NULL;
+    printf ("passagem\n");
+    ptr_mat->prim = allocation_memory(ptr_mat->prim);
+    printf ("passagem\n");
+    struct lista *aux = ptr_mat->prim;
+    printf ("passagem\n");
+    int flag=1;
+    printf("\nDIGITE APENAS OS DADOS DOS TERMOS DIFERENTES DE ZERO DA SUA MATRIZ\n");
+
+    while(flag==1){
+
+        printf("Digite linha: ");
+        scanf("%d",&aux->row);
+        printf("Digite coluna: ");
+        scanf("%d",&aux->column); 
+        printf("Digite o valor: ");
+        scanf("%d",&aux->value);
+
+        printf("Se existir mais um termo a ser informado DIGITE 1, caso contrario digite outro número!");
+        scanf("%d",&flag);
+
+        if(flag==1)
+            aux = aux->next;
+        else 
+            aux->next = NULL;
+
+    }
+    return ptr_mat;
+
+}
+
+struct identidade *preenche_matrizz(struct identidade *ptr_mat, int ordem){
+
+    ptr_mat = le_dados(ptr_mat,ordem);
+
+}
+
+int le_ordem(){
+    int ordem;
+    printf("Digite a ordem de sua matriz identidade: ");
+    scanf("%d",&ordem);
+    return ordem;
+}
+
+/*
+struct lista *create_matriz(struct lista *ptr_mat, int ordem){
     
     ptr_mat = allocation_memory(ptr_mat);
-    struct identidade *aux = ptr_mat;
+    struct lista *aux = ptr_mat;
 
-    for(int i=1;i < (ordem*ordem) ;i++){
+    for(int i=0;i < (ordem*ordem) ;i++){
         if((i+1) != (ordem*ordem) ){
             aux->next = allocation_memory(aux->next);
         }else{
@@ -46,17 +92,17 @@ struct identidade *create_matriz(struct identidade *ptr_mat, int ordem){
 
 }
 
-struct identidade *preenche_matriz(struct identidade *ptr_mat){
+struct lista *preenche_matriz(struct lista *ptr_mat){
 
-    struct identidade *aux = ptr_mat;
+    struct lista *aux = ptr_mat;
 
     while(aux != NULL){
         printf("Digite linha: ");
-        scanf("%d",&ptr_mat->row);
+        scanf("%d",&aux->row);
         printf("Digite coluna: ");
-        scanf("%d",&ptr_mat->column);
+        scanf("%d",&aux->column); 
         printf("Digite o valor: ");
-        scanf("%d",&ptr_mat->value);
+        scanf("%d",&aux->value);
 
         aux = aux->next;
     }
@@ -65,13 +111,38 @@ struct identidade *preenche_matriz(struct identidade *ptr_mat){
 
 }
 
+void imprime_matriz(struct lista *ptr_mat,int ordem){
+
+    struct lista *aux = ptr_mat;
+
+    printf("\n");
+    for(int i=0;i<ordem;i++){
+        for(int j=0;j<ordem;j++){
+            printf("%d\t",aux->value);
+            aux = aux->next;
+        }
+        printf("\n");
+    }
+}
+
+*/
+
 int main()
 {
     struct identidade *ptr_mat = NULL; 
-           
+
     int ordem = le_ordem();
-    ptr_mat = create_matriz(ptr_mat,ordem);
-    ptr_mat = preenche_matriz(ptr_mat);
+
+    ptr_mat = preenche_matrizz(ptr_mat,ordem);
+
+
+
+
+
+
+    //ptr_mat = create_matriz(ptr_mat,ordem);
+    //ptr_mat = preenche_matriz(ptr_mat);
+    //imprime_matriz(ptr_mat,ordem);
 
 
     return 0;
